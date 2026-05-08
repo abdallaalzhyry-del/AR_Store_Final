f8f91bd844378ffedbc5ca0846c1dff90eb5be7b
abdallaalzhry-del <abdallaalzhry@gmail.com>
2026-05-08 04:29:39 +0300

    Force rebuild for Vercel

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -87,4 +87,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'# Deployment trigger
