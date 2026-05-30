64b452615208b600d1e43c99569c0a2ac6bf8ca3
abdallaalzhry-del <abdallaalzhry@gmail.com>
2026-05-30 18:31:20 +0300

    fix: explicit app definition for vercel and whitenoise

--- my_shop/wsgi.py
+++ my_shop/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for my_shop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
+# تأكد من اسم المشروع بتاعك (my_shop)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_shop.settings')
 
+# تعريف الـ application الأساسي
 application = get_wsgi_application()
+
+# إضافة دعم WhiteNoise لملفات الـ Static
+application = WhiteNoise(application)
+
+# تعريف متغير 'app' لبيئة Vercel (مهم جداً لتجنب إيرور 500)
+app = application
